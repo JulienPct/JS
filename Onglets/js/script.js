0859de2789e166c52db76fd132586eac3d4bfd24
JulienPct <pactat.julien@gmail.com>
2020-12-22 21:21:36 +0100

    Onglets

---
+++ Onglets/js/script.js
@@ -0,0 +1,68 @@
+(function(){
+
+    var afficherOnglet = function(a, animations){
+
+        if(animations === undefined)
+            animations = true;
+
+        var div = a.parentNode.parentNode.parentNode
+        var li = a.parentNode
+        var activeTab = div.querySelector('.tab_content.active')
+        var aAfficher = div.querySelector(a.getAttribute('href'))
+    
+        if(li.classList.contains('active'))
+            return false
+    
+        div.querySelector('.tabs .active').classList.remove('active')
+        li.classList.add('active')
+        //div.querySelector('.tab_content.active').classList.remove('active')
+        //div.querySelector(a.getAttribute('href')).classList.add('active')
+        
+        if (animations){
+            activeTab.classList.add('fade')
+            activeTab.classList.remove('in')
+
+            var transitionEnd = function(){
+                this.classList.remove('fade')
+                this.classList.remove('active')
+                aAfficher.classList.add('active')
+                aAfficher.classList.add('fade')
+                aAfficher.offsetWidth
+                aAfficher.classList.add('in')
+                activeTab.removeEventListener('transitionend', transitionEnd)
+            }
+
+            activeTab.addEventListener('transitionend', transitionEnd)
+            activeTab.addEventListener('webkittransitionend', transitionEnd)
+            activeTab.addEventListener('otransitionend', transitionEnd)
+        }
+        else {
+            aAfficher.classList.add('active')
+            activeTab.classList.remove('active')
+        }
+
+    }
+    
+    var tabs = document.querySelectorAll('.tabs a')
+    
+    for (var i = 0 ; i < tabs.length; i++){
+        tabs[i].addEventListener('click', function(e){
+            afficherOnglet(this)
+        })
+    }
+    
+    var hashchange = function(e){
+
+        var hash = window.location.hash
+        var a = document.querySelector('a[href="' + hash + '"]')
+        
+        if (a != null && !a.classList.contains('active')){
+            afficherOnglet(a, e!==undefined)
+        }
+
+    }
+
+    window.addEventListener('hashchange', hashchange)
+    hashchange()
+
+})()
